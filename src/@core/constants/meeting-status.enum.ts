49614d014841185782e50af46303435f9ec9221c
manhluong1999 <manhqthp1999@gmail.com>
2022-08-12 23:58:25 +0700

    fix meeting status

--- src/@core/constants/meeting-status.enum.ts
+++ src/@core/constants/meeting-status.enum.ts
@@ -2,8 +2,7 @@ export enum MeetingStatus {
   PENDING = 0,
   APPROVED = 1,
   PAID = 2,
-  MEETING = 3,
-  FINISHED = 4,
-  NOT_APPROVED = 5,
-  NOT_FINISHED = 6,
+  FINISHED = 3,
+  NOT_APPROVED = 4,
+  NOT_FINISHED = 5,
 }
