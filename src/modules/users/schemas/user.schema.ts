323ca674d84a258974a86707f30221cf5f4bda6c
manhluong1999 <manhqthp1999@gmail.com>
2022-08-13 00:08:31 +0700

    update

--- src/modules/users/schemas/user.schema.ts
+++ src/modules/users/schemas/user.schema.ts
@@ -50,7 +50,10 @@ export class User {
   @Prop()
   address: string;
 
-  @Prop()
+  @Prop({
+    default:
+      'https://st3.depositphotos.com/1767687/16607/v/450/depositphotos_166074422-stock-illustration-default-avatar-profile-icon-grey.jpg',
+  })
   imgUrl: string;
 
   @Prop()
